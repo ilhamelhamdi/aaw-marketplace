31519ed9d52d610b02f25d99116c5afded8d71b8
Ilham Abdillah Alhamdi <ilham.abdillah@ui.ac.id>
2025-03-07 21:45:05 +0700

    add workload test script

---
+++ workload_test/seeds/populate_users.py
@@ -0,0 +1,32 @@
+import requests
+import concurrent.futures
+
+# Base URL for the API
+url = "http://localhost:8001/api/register"
+
+# Function to generate user data
+def generate_user_data(user_id):
+    return {
+        "username": f"user{user_id}",
+        "email": f"user{user_id}@gmail.com",
+        "password": f"UserUser{user_id}",
+        "full_name": f"User{user_id}",
+        "address": "Blablabla",
+        "phone_number": "081234567890"
+    }
+
+# Function to send a POST request for a given user ID
+def register_user(user_id):
+    try:
+        user_data = generate_user_data(user_id)
+        requests.post(url, json=user_data)
+        if user_id % 100 == 0:
+            print(f"User {user_id}: Done")
+        # print(f"User {user_id}: {response.status_code}")
+    except Exception as e:
+        pass
+        # print(f"User {user_id}: {e}")
+
+# Use ThreadPoolExecutor to send requests concurrently
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+    executor.map(register_user, range(1000, 10001))
